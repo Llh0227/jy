b4288a988a778212f592bc400ad1666fb2a28155
Llh0227 <102984335+Llh0227@users.noreply.github.com>
2025-11-02 01:26:37 +0800

    Update VolumeController.kt

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/VolumeController.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/VolumeController.kt
@@ -1,4 +1,4 @@
-package com.jyycxz.joy
+package com.zhengneng.cable
 
 // Inspired by https://github.com/yosemiteyss/flutter_volume_controller/blob/main/android/src/main/kotlin/com/yosemiteyss/flutter_volume_controller/VolumeController.kt
 
